0c5f70f808ca5c9db2a866065eff75990d60d46b
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2015-08-04 18:14:00 +0200

    First commit:

---
+++ ticketswap_crawler/ticketswap_crawler/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for ticketswap_crawler project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'ticketswap_crawler'
+
+SPIDER_MODULES = ['ticketswap_crawler.spiders']
+NEWSPIDER_MODULE = 'ticketswap_crawler.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'ticketswap_crawler (+http://www.yourdomain.com)'
